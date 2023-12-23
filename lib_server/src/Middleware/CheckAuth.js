1457b1264758a65c44582afddf879e12f3c10c2c
JIJIN12 <jijinsuresh6@gmail.com>
2023-12-23 11:41:19 +0530

    login updated

--- lib_server/src/Middleware/CheckAuth.js
+++ lib_server/src/Middleware/CheckAuth.js
@@ -1,17 +1,20 @@
-const jwt = require('jsonwebtoken')
-
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    try {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
 
-        const token = req.headers.authorization.split(" ")[1]
+    const decode_token = jwt.verify(token, "divide&rule");
 
-        const decode_token = jwt.verify(token, 'divide&rule');
-
-console.log(decode_token);
-        req.userData = { username: decode_token.username, password: decode_token.password, role: decode_token.role, id: decode_token.id }
-        next();
-    } catch (error) {
-        return res.status(400).json({ message: 'Authentication failed' })
-    }
-}
+    console.log(decode_token);
+    req.userData = {
+      username: decode_token.username,
+      password: decode_token.password,
+      role: decode_token.role,
+      id: decode_token.id,
+    };
+    next();
+  } catch (error) {
+    return res.status(400).json({ message: "Authentication failed" });
+  }
+};
